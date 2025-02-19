c03f778bbac17608fed0a861461ea0d6f6c94fad
petersonbabs <timiebabs@gmail.com>
2025-02-19 12:49:53 +0100

    dev

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,14 +1,76 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import {io} from "socket.io-client"
 
-const socket = io("http://localhost:4005")
+const socket = io(import.meta.env.VITE_BASE_URL)
 
 const Chat = () => {
-    useEffect(()=>{
-        console.log(socket)
-    },[socket])
+    const [message, setMessage] = useState("")
+    const [messages, setMessages] = useState([]);
+
+
+    const handleInput = (e)=>{
+        setMessage(e.target.value)
+    }
+
+    useEffect(() => {
+        // Listen for incoming messages
+        socket.on("receiveMessage", (newMessage) => {
+          setMessages((prev) => [...prev, newMessage]);
+        });
+    
+        return () => {
+          socket.off("receiveMessage"); // Cleanup listener on unmount
+        };
+      }, []);
+
+    const sendMessage =(e)=>{
+        e.preventDefault()
+        console.log(message);
+        socket.emit("sendMessage", message)
+    }
   return (
-    <div>Chat</div>
+    <div style={{
+        width:"90vw",
+        maxWidth: "600px",
+        margin: "2rem auto",
+        display: "flex",
+        flexDirection: "column",
+        height: "80vh",
+        
+    }}>
+        <div style={{
+            flex: 1,
+            padding: "1rem",
+            height: "300px",
+            border: "1px solid",
+            marginBottom:"1rem"
+        }}>
+            {
+                messages?.map((message, index) => (
+                    <p key={index} style={{background: "#333",
+                        width: "50%",
+                        padding: "1rem",
+                        borderRadius: "5px",
+                        color: 'white'
+                    }}>{message}</p>
+                ))
+            }
+        </div>
+        <form
+        onSubmit={sendMessage}
+         style={{
+            display:"flex",
+            gap: "1rem"
+        }}>
+            <input 
+            onChange={handleInput}
+            style={{
+                flex:1,
+                padding: ".5rem 1rem"
+            }} type="text" name="message" id="message" placeholder="Start typing..." />
+            <button>Send</button>
+        </form>
+    </div>
   )
 }
 
